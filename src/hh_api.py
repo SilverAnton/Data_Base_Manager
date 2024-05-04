66c19c14b8d07dfc59903247afff861c7c985167
SilverAnton <cu4hawk@gmail.com>
2024-05-04 16:10:15 +0700

    add code

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,39 +1,35 @@
 import requests
+import json
 
 
 class HeadHunterApi:
     def __init__(self):
         self.__url = "http://api.hh.ru/employers/"
-        self.__param = {'text': '', "per_page": 10, "sort_by": "by_vacancies_open"}
+        self.__param = {"text": '', "per_page": 10, "sort_by": "by_vacancies_open"}
 
         self.employers = []
         self.vacancies = []
         self.result = {}
 
-
     def add_employers(self, keyword):
         """Метод добавляет список словарей с информацией о работодателях, по ключевому слову в атрибут employers"""
         self.__param['text'] = keyword
         response = requests.get(self.__url, self.__param)
-        self.employers.append(response.json().get("items"))
+        self.employers.append(response.json()["items"])
 
-
-
-    def add_vacancy(self, keyword):
+    def add_vacancy(self):
         employers = self.employers[0]
-        self.__param['text'] = keyword
-        self.__param['per_page'] = 20
+        del self.__param['text']
+        del self.__param["sort_by"]
+        self.__param['per_page'] = 100
         for employer in employers:
             response = requests.get(employer['vacancies_url'], self.__param)
             self.vacancies.append(response.json()['items'])
 
+    def save_as_json_employers(self):
+        with open('employers.json', 'w', encoding="UTF8") as file:
+            json.dump(self.employers, file, ensure_ascii=False, indent=4)
 
-
-
-
-obj = HeadHunterApi()
-obj.add_employers('develop')
-obj.add_vacancy('python')
-#print(len(obj.vacancies))
-print(obj.employers[0])
-
+    def save_as_json_vacancies(self):
+        with open('vacancies.json', 'w', encoding="UTF8") as file:
+            json.dump(self.vacancies, file, ensure_ascii=False, indent=4)
